4d75aeacfd00460fffe7a9cec22073c4f2256464
emlynw <emlynw98@gmail.com>
2025-04-11 12:03:04 +0100

    keyboard teleop uses same wrappers as gamepad

--- fruit_gym/test/keyboard_mouse_teleop.py
+++ fruit_gym/test/keyboard_mouse_teleop.py
@@ -4,6 +4,8 @@ import cv2
 from fruit_gym import envs
 import numpy as np
 import time
+import os
+from wrappers import VideoRecorder, RotateImage, SERLObsWrapper
 
 # Global variables to capture mouse movement
 mouse_x, mouse_y = 0, 0  # Track mouse position
@@ -14,40 +16,55 @@ def mouse_callback(event, x, y, flags, param):
         mouse_x, mouse_y = x, y
 
 def main():
-    render_mode = "rgb_array"
-    env = gym.make("PickStrawbEnv", render_mode=render_mode, randomize_domain=True, reward_type="sparse", ee_dof=4)
-    env = TimeLimit(env, max_episode_steps=500)    
-    waitkey = 50
-    resize_resolution = (480, 480)
+    record = True
+    camera_res = 480
+    video_res = 480
+    cameras = ['wrist1', 'wrist2']
+    proprio_keys = ["tcp_pose", "gripper_pos"]
+    display_res = (640, 640)
+    fps = 20  # Frames per second for video recording
+    num_episodes = 10
+    dir = os.path.dirname(__file__)
+    video_dir = os.path.join(dir, 'videos')
+    waitkey = 10
+
+    env = gym.make("PickStrawbEnv", randomize_domain=True, reward_type="sparse", ee_dof=4, width=camera_res, height=camera_res, gripper_pause=False)
+    env = TimeLimit(env, max_episode_steps=500)
+    env = SERLObsWrapper(env, proprio_keys=proprio_keys)
+    env = RotateImage(env, pixel_key="wrist1")
+    if record:
+        for image_name in cameras:
+                crop_res = env.observation_space[image_name].shape[0]
+                env = VideoRecorder(env, video_dir, camera_name=image_name, crop_resolution=crop_res, resize_resolution=video_res, fps=fps, record_every=1)
 
     # Define the range for absolute movement control
-    max_speed = 1.0  # Maximum speed in any direction
-    rot_speed = 1.0  # Maximum rotation speed
+    max_speed = 0.5  # Maximum speed in any direction
+    rot_speed = 0.5  # Maximum rotation speed
 
     # Set up mouse callback
-    cv2.namedWindow("wrist2")
-    cv2.setMouseCallback("wrist2", mouse_callback)
+    cv2.namedWindow("wrist1")
+    cv2.setMouseCallback("wrist1", mouse_callback)
     
-    while True:
+    for episode in range(num_episodes):
         terminated = False
         truncated = False
         obs, info = env.reset()
-        i=0
+        print(f"Press any key to start episode {episode}")
+        for camera in cameras:
+            frame = cv2.resize(cv2.cvtColor(obs[camera], cv2.COLOR_RGB2BGR), display_res)
+            cv2.imshow(camera, frame)
+        cv2.waitKey(0)  # Wait for a key press to begin the episode
         
         while not (terminated or truncated):
             step_start_time = time.time()
-            i+=1
             # Display the environment
-            if render_mode == "rgb_array":
-                cv2.imshow("wrist2", cv2.resize(cv2.cvtColor(obs['images']['wrist2'], cv2.COLOR_RGB2BGR), resize_resolution))
-                # Rotate wrist1 by 180 degrees
-                wrist1 = cv2.rotate(obs['images']['wrist1'], cv2.ROTATE_180)
-                cv2.imshow("wrist1", cv2.resize(cv2.cvtColor(wrist1, cv2.COLOR_RGB2BGR), resize_resolution))
-                cv2.imshow("front", cv2.resize(cv2.cvtColor(obs["images"]["front"], cv2.COLOR_RGB2BGR), resize_resolution))
+            for camera in cameras:
+                frame = cv2.resize(cv2.cvtColor(obs[camera], cv2.COLOR_RGB2BGR), display_res)
+                cv2.imshow(camera, frame)
             
             # Calculate movement based on absolute mouse position within window
-            move_left_right = ((mouse_x / resize_resolution[0]) * 2 - 1) * max_speed
-            move_up_down = -((mouse_y / resize_resolution[1]) * 2 - 1) * max_speed
+            move_left_right = ((mouse_x / display_res[0]) * 2 - 1) * max_speed
+            move_up_down = -((mouse_y / display_res[1]) * 2 - 1) * max_speed
 
             # Define movement actions for W and S keys (forward/backward)
             key = cv2.waitKey(waitkey) & 0xFF
